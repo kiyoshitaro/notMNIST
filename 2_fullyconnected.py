20c8afb730b809f2cf40b225630ff6d4c1ed3cbb
Nguyễn Tuấn Hưng <hunglhp1998@gmail.com>
2018-10-13 19:04:51 +0700

    ReLU layer

--- 2_fullyconnected.py
+++ 2_fullyconnected.py
@@ -47,48 +47,63 @@ print('Test set', test_dataset.shape, test_labels.shape)
 #BUILD GRAPH
 
 train_subset = 10000
+
 graph = tf.Graph()
 with graph.as_default():
-    #load data into constant to attach graph
-    tf_train_dataset = tf.constant(train_dataset[:train_subset,:])
-    tf_test_dataset = tf.constant(test_dataset)
-    tf_valid_dataset = tf.constant(valid_dataset)
-    tf_train_labels = tf.constant(train_labels[:train_subset])
-    
-    # Variables.
-    weight = tf.Variable(tf.truncated_normal([image_size*image_size, num_labels])) #random values following a (truncated)
 
-    biases = tf.Variable(tf.zeros([num_labels]))
-    
-    #computation
-    logits = tf.matmul(tf_train_dataset,weight) + biases
-    
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = tf_train_labels,logits = logits))
-    
-    #Optimizer
-    
-    optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
-    
-    #Predict
-    
-    train_predict = tf.nn.softmax(logits)
-    valid_predict = tf.nn.softmax(tf.matmul(tf_valid_dataset,weight)+biases)
-    test_predict = tf.nn.softmax(tf.matmul(tf_test_dataset,weight)+biases)
-    
-#ITERATE:
+    #load data into constant to attach graph
+  tf_train_dataset = tf.placeholder(tf.float32,
+                                    shape=(batch_size, image_size * image_size))
+  tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+  tf_valid_dataset = tf.constant(valid_dataset)
+  tf_test_dataset = tf.constant(test_dataset)
+  
+  # Variables.
+  weights1 = tf.Variable(
+    tf.truncated_normal([image_size * image_size, num_hidden_nodes]))
+  biases1 = tf.Variable(tf.zeros([num_hidden_nodes]))
+  weights2 = tf.Variable(
+    tf.truncated_normal([num_hidden_nodes, num_labels]))
+  biases2 = tf.Variable(tf.zeros([num_labels]))
+  
+   #Computation
+  lay1_train = tf.nn.relu(tf.matmul(tf_train_dataset, weights1) + biases1)
+  logits = tf.matmul(lay1_train, weights2) + biases2
+  loss = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels))
+  
+  # Optimizer.
+  optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+  
+  # Predict
+  train_prediction = tf.nn.softmax(logits)
+  lay1_valid = tf.nn.relu(tf.matmul(tf_valid_dataset, weights1) + biases1)
+  valid_prediction = tf.nn.softmax(tf.matmul(lay1_valid, weights2) + biases2)
+  lay1_test = tf.nn.relu(tf.matmul(tf_test_dataset, weights1) + biases1)
+  test_prediction = tf.nn.softmax(tf.matmul(lay1_test, weights2) + biases2)
+
+
 num_steps = 800
 def accuracy(predictions, labels):
     return(100.0*(np.sum(np.argmax(predictions,1) == np.argmax(labels,1))/predictions.shape[0]))
 
-with tf.Session(graph = graph) as session:
-    tf.global_variables_initializer().run()
-    print("INIt")
-    for step in range(num_steps):
-        _,l,predictions = session.run([optimizer,loss,train_predict])
-        if( step %100 ==0):
-            print('Loss at step %d: %f' % (step, l))
-            print('Training accuracy: %.1f%%' % accuracy(predictions, train_labels[:train_subset, :]))
-            print('Validation accuracy: %.1f%%' % accuracy(valid_prediction.eval(), valid_labels))
-     print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
+with tf.Session(graph=graph) as session:
+  tf.initialize_all_variables().run()
+  print("Initialized")
+    #ITERATE:
+  for step in range(num_steps):
+    offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+    # Generate a minibatch.
+    batch_data = train_dataset[offset:(offset + batch_size), :]
+    batch_labels = train_labels[offset:(offset + batch_size), :]
+    feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
+    _, l, predictions = session.run(
+      [optimizer, loss, train_prediction], feed_dict=feed_dict)
+    if (step % 500 == 0):
+      print("Minibatch loss at step %d: %f" % (step, l))
+      print("Minibatch accuracy: %.1f%%" % accuracy(predictions, batch_labels))
+      print("Validation accuracy: %.1f%%" % accuracy(
+        valid_prediction.eval(), valid_labels))
+  print("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(), test_labels))
     
     
